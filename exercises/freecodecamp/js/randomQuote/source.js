c2b2e2ab2fb2aad6154da8f9d928329c1b1ef245
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-10-09 10:40:18 +0300

    finished with random quote mashine for freeCodeCamp

--- exercises/freecodecamp/js/randomQuote/source.js
+++ exercises/freecodecamp/js/randomQuote/source.js
@@ -4,14 +4,14 @@
  * without completely redirecting the page. */
 function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
 
-const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
-const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+const COLORS = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 
-function getServerQuotes(url){
+function getQuotes(URL){
     return new Promise ((resolve, reject) => {
         const request = new XMLHttpRequest();
-        request.open('GET', url);
+        request.open('GET', URL);
         request.onload = () => (request.status === 200)? resolve(request.response) : reject(Error(request.statusText));
         request.onerror = () => reject(Error('Network error'));
         request.send();
@@ -19,23 +19,25 @@ function getServerQuotes(url){
     })
 }
 
-function getJsonQuotes(url){
-    return getServerQuotes(url).then(JSON.parse)
+function getJsonQuotes(URL){
+    return getQuotes(URL).then(JSON.parse)
 }
 
-function getQuotes(){
-    getJsonQuotes(url)
+function setQuote(){
+    getJsonQuotes(URL)
         .then( result => {
             // console.log(result);
-            let quote = getRandomQuote(result);
-            let elemText = document.getElementById('text');
-            let elemAuthor = document.getElementById('author');
-            if (quote){
-                elemText.innerHTML += `${quote.quote}`;
-                elemAuthor.innerHTML = `- ${quote.author}`;
+            let currentQuote = getRandomQuote(result);
+            if (currentQuote){
+                let twit = encodeURIComponent(`"${currentQuote.quote} ${currentQuote.author}"`);
+                let twitHref = document.getElementById('tweet-quote').getAttribute('href');
+                document.getElementById('tweet-quote').setAttribute('href', twitHref+`?hashtags=quotes&related=freecodecamp&text='${twit}`);
+                document.getElementById('quote').innerHTML = `${currentQuote.quote}`;
+                document.getElementById('author').innerHTML = `- ${currentQuote.author}`;
+                setTimeout(()=>document.getElementById('quote-message').style.opacity = 1, 500)
             }
             else{
-                elemText.innerHTML = "Can't load quote";
+                document.getElementById('quote').innerHTML = "Can't load quote";
             }
         })
         .catch((err) => {
@@ -49,38 +51,35 @@ function getRandomQuote(quotes){
     return quotes? quotes.quotes[Math.floor(Math.random()*quotes.quotes.length)] : {};
 }
 
-function getRandomColor(colors){
-    return colors[Math.floor(Math.random()*colors.length)]
+function getRandomColor(COLORS){
+    return COLORS[Math.floor(Math.random()*COLORS.length)]
 }
 function colorize(){
-    let color = getRandomColor(colors);
-    let elemStyle = document.body.style;
-    if (elemStyle.backgroundColor !== color){
-        let int = setInterval(()=>{
-            if (elemStyle.backgroundColor !== color){
-                elemStyle.backgroundColor = color;
-                elemStyle.color = color;
-            }
-            else{
-                clearInterval(int);
-            }
-        }, 5000)
-        // elemStyle.backgroundColor = color;
-        // elemStyle.color = color;
-        // document.getElementById('quote-box').style.color = color;
-        // transition(document.body);
-
+    let color = getRandomColor(COLORS);
+    let bodyStyle = document.body.style;
+    if (bodyStyle.color !== color){
+        bodyStyle.backgroundColor = color;
+        bodyStyle.color = color;
+        document.getElementById('new-quote').style.backgroundColor = color;
+        [...document.getElementsByClassName('button')].forEach(el => el.style.color = color)
     }
     else{
         colorize();
     }
 }
-function transition(elem){
-    elem.style.transition = 'all 5s ease'
-}
 document.addEventListener('DOMContentLoaded', () => {
     colorize();
-})
+});
+
 window.onload = () => {
-    getQuotes();
+    setQuote();
+    document.getElementById('new-quote').addEventListener('click', () => {
+        document.getElementById('quote-message').style.opacity = 0, 500
+        setTimeout(() => {
+            setQuote();
+            colorize();
+        }, 1200)
+    });
 }
+
+    
