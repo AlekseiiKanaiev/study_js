d70643fe850e8580dee5d3080ce5e2c7b9f23ad6
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-11-21 12:04:53 +0200

    finished 9-classes section

---
+++ learn-js/1-JS/9-classes/7-mixins/7-mixins.js
@@ -0,0 +1,56 @@
+let eventMixin = {
+   /**
+   * Подписаться на событие, использование:
+   * menu.on('select', function(item) { ... }
+   */
+    on(eventName, handler) {
+        if(!this._eventHandlers) this._eventHandlers = {};
+        if(!this._eventHandlers[eventName]) this._eventHandlers[eventName] = [];
+        this._eventHandlers[eventName].push(handler)
+    },
+
+    /**
+   * Отменить подписку, использование:
+   * menu.off('select', handler)
+   */
+    off(eventName, handler) {
+        let handlers = this._eventHandlers && this.eventName[eventName];
+        if (!handlers) return;
+        for (let i = 0; i < handler.length; i++){
+            if (handlers[i] === handler) {
+                handlers.splice(i--, 1)
+            }
+        }
+    },
+
+    /**
+   * Сгенерировать событие с указанным именем и данными
+   * this.trigger('select', data1, data2);
+   */
+    trigger(eventName, ...args) {
+        if(!this._eventHandlers || !this._eventHandlers[eventName]){
+            return; // обработчиков для этого события нет
+        }
+        // вызовем обработчики
+        this._eventHandlers[eventName].forEach(handler => {
+            handler.apply(this, args);
+        });
+    }
+};
+
+
+// Создадим класс
+class Menu {
+    choose(value) {
+        this.trigger("select", value);
+    }
+}
+// Добавим примесь с методами для событий
+Object.assign(Menu.prototype, eventMixin);
+
+let menu = new Menu();
+
+// Добавить обработчик, который будет вызван при событии "select":
+menu.on('select', value => console.log(`You choose: ${value}`));
+
+menu.choose('123')
