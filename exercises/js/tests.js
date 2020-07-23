8c3c4f1da0a44e87a7111d4796166ce5c32d5abc
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2020-07-23 09:30:10 +0300

    mod tests

--- exercises/js/tests.js
+++ exercises/js/tests.js
@@ -50,9 +50,35 @@ function f1(n){
 }
 // console.log(f1(103));
 
-var name = 'Alex';
-var getName = name;
-
 // console.log(window.getName);
 // console.log(this.getName);
 
+let arr = [];
+
+arr = arr.concat(['a', 'b'])
+arr = arr.concat(['a', 'b'])
+// console.log(arr);
+
+let r = {}
+// arr.splice(0, 0, 'b');
+
+// console.log(arr);
+
+arr.splice(4, 0, 'c');
+// console.log(arr.length);
+
+
+let name = 'alex';
+
+name = name.replace(name[0], name[0].toUpperCase());
+
+console.log(name);
+
+let o1 = {a: false, b: true};
+
+Object.assign(o1, {a: true});
+console.log(o1);
+
+// let arr1 = [1, 5, 6 , 4]
+
+// console.log(arr1.slice(0, 2));
